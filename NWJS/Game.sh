b3983113e0e437e8b7cd6d4c1f6db949dd314f86
Revival8697 <24849056+Revival8697@users.noreply.github.com>
2024-02-26 01:53:05 +0700

    Add error checking

--- NWJS/Game.sh
+++ NWJS/Game.sh
@@ -1,31 +1,65 @@
 #!/usr/bin/env bash
 
+set -e
+
+command -v attr >/dev/null 2>&1 || { echo >&2 "attr is required but it's not installed. Aborting."; exit 1; }
+
 if command -v fusermount > /dev/null 2>&1
 then
     echo "FUSE found."
-    www_dir=$(find . -type d -name "www" -print -quit)
-    if [ -n "$www_dir" ]
+    if [ -d ./www ]
     then
         echo "'www' directory found."
-        cp ./package.json ../NWJS/package.json & wait $!
+        if cp ./package.json ../NWJS/package.json
+        then
+            echo "Copied package.json successfully."
+        else
+            echo "Failed to copy package.json. Exiting."; exit 1
+        fi
         echo "Mounting..."
-        ../NWJS/lib/cicpoffs ./www ../NWJS/www
+        if ../NWJS/lib/cicpoffs ./www ../NWJS/www
+        then
+            echo "Mounted successfully."
+        else
+            echo "Failed to mount. Exiting."; exit 1
+        fi
+    else
+        echo "'www' directory not found. Exiting."; exit 1
     fi
-else echo "FUSE not found. Game will launch as case sensitive."
+else
+    echo "FUSE not found. Game will launch as case sensitive."
 fi
 
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]
 then
     echo "Launching as a Wayland application."
-#     ../NWJS/nw --ozone-platform=wayland; # Performance degradation
-    ../NWJS/nw --ozone-platform=x11
-else echo "Launching as a X11 application."; ../NWJS/nw --ozone-platform=x11
+    # ../NWJS/nw --ozone-platform=wayland; # Performance degradation
+    if ! ../NWJS/nw --ozone-platform=x11
+    then
+        echo "Failed to launch. Exiting."; exit 1
+    fi
+else
+    echo "Launching as a X11 application."
+    if ! ../NWJS/nw --ozone-platform=x11
+    then
+        echo "Failed to launch. Exiting."; exit 1
+    fi
 fi
 
 if command -v fusermount > /dev/null 2>&1
 then
     echo "Unmounting..." &
-    fusermount -u ../NWJS/www & wait $!
-    rm ../NWJS/package.json
+    if fusermount -u ../NWJS/www
+    then
+        echo "Unmounted successfully."
+    else
+        echo "Failed to unmount. Exiting."; exit 1
+    fi
+    if rm ../NWJS/package.json
+    then
+        echo "Removed package.json successfully."
+    else
+        echo "Failed to remove package.json. Exiting."; exit 1
+    fi
 fi
 echo "Finished."
--- optimize.sh
+++ optimize.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
 
+set -e
+
+command -v pngcheck >/dev/null 2>&1 || { echo >&2 "pngcheck is required but it's not installed. Aborting."; exit 1; }
+command -v oxipng >/dev/null 2>&1 || { echo >&2 "oxipng is required but it's not installed. Aborting."; exit 1; }
+command -v cwebp >/dev/null 2>&1 || { echo >&2 "cwebp is required but it's not installed. Aborting."; exit 1; }
+command -v parallel >/dev/null 2>&1 || { echo >&2 "parallel is required but it's not installed. Aborting."; exit 1; }
+
 optimize_images()
 {
     file="$1"
     if pngcheck -v "$file" | grep -q -E "acTL" # Check if a file is an APNG file
     then
-        oxipng --opt max --strip safe "$file"
+        oxipng --opt max --strip safe "$file" || echo >&2 "oxipng failed on $file. Continuing"
     else
-        cwebp -lossless "$file" -o "$file"
+        cwebp -lossless "$file" -o "$file" || echo >&2 "cwebp failed on $file. Continuing."
     fi
 }
 
 export -f optimize_images
 
-cp -R ./www ./www_backup & wait $!
+[ -d ./www ] || { echo >&2 "./www doesn't exist. Aborting."; exit 1; }
 
-oxipng --opt max --strip all ./www/icon/icon.png & wait $!
+if [ ! -d ./www_backup ]
+then
+    cp -R ./www ./www_backup
+else
+    echo "./www_backup already exists. Skipping backup."
+fi
+
+if [ -f ./www/icon/icon.png ]
+then
+    oxipng --opt max --strip all ./www/icon/icon.png
+else
+    echo >&2 "./www/icon/icon.png doesn't exist. Ignoring."
+fi
 
 find ./www -type f -name "*.png" ! -name "icon.png" | parallel optimize_images
+
+echo "Finished."
