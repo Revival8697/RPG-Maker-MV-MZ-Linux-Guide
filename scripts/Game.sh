66a96ef94901ffd15d34c6c5a30375642c07f0cd
Revival8697 <24849056+Revival8697@users.noreply.github.com>
2024-02-29 15:29:57 +0700

    Reorganize scripts

--- scripts/Game.sh
+++ scripts/Game.sh
@@ -8,7 +8,7 @@ command -v cicpoffs >/dev/null 2>&1 || { echo -e >&2 "cicpoffs is required but i
 if [ -d ./www ]
 then
     echo "'www' directory found."
-    if cp ./package.json ../nwjs/package.json
+    if cp ./package.json $XDG_DATA_HOME/porter/nwjs/package.json
     then
         echo "Copied package.json successfully."
     else
@@ -16,7 +16,7 @@ then
     fi
 
     echo "Mounting..."
-    if cicpoffs ./www ../nwjs/www
+    if cicpoffs ./www $XDG_DATA_HOME/porter/nwjs/www
     then
         echo "Mounted successfully."
     else
@@ -30,29 +30,29 @@ fi
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]
 then
     echo "Launching as a Wayland application."
-    # ../nwjs/nw --ozone-platform=wayland; # Performance degradation
-    if ! ../nwjs/nw --ozone-platform=x11
+    # $XDG_DATA_HOME/porter/nwjs/nw --ozone-platform=wayland; # Performance degradation
+    if ! $XDG_DATA_HOME/porter/nwjs/nw --ozone-platform=x11
     then
         echo "Failed to launch. Exiting."; exit 1
     fi
 
 else
     echo "Launching as a X11 application."
-    if ! ../nwjs/nw --ozone-platform=x11
+    if ! $XDG_DATA_HOME/porter/nwjs/nw --ozone-platform=x11
     then
         echo "Failed to launch. Exiting."; exit 1
     fi
 fi
 
 echo "Unmounting..."
-if fusermount -u ../nwjs/www
+if fusermount -u $XDG_DATA_HOME/porter/nwjs/www
 then
     echo "Unmounted successfully."
 else
     echo "Failed to unmount. Exiting."; exit 1
 fi
 
-if rm ../nwjs/package.json
+if rm $XDG_DATA_HOME/porter/nwjs/package.json
 then
     echo "Removed package.json successfully."
 else
---
+++ scripts/nwjs-manager.sh
@@ -0,0 +1,71 @@
+#!/bin/env bash
+
+set -e
+
+command -v curl >/dev/null 2>&1 || { echo >&2 "curl is required but it's not installed. Aborting."; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required but it's not installed. Aborting."; exit 1; }
+
+URL="https://nwjs.io/versions"
+TMP_FILE="/tmp/versions.json"
+CURRENT="$XDG_DATA_HOME/porter/nwjs-version.txt"
+
+download() {
+    local version=$1
+    local url="https://dl.nwjs.io/${version}/nwjs-sdk-${version}-linux-x64.tar.gz"
+    local tmp_file="/tmp/nwjs.tar.gz"
+    local extract_dir="/tmp/nwjs-sdk-${version}-linux-x64"
+    local target_dir="$XDG_DATA_HOME/porter/nwjs"
+
+    echo -e "\nDownloading $version:"
+    if curl -fSL -o "$tmp_file" "$url" && tar -xzf "$tmp_file" -C "/tmp"
+    then
+        rm -rf "$target_dir"
+        mv "$extract_dir" "$target_dir"
+        mkdir "$target_dir"/www
+        cp ./Game.sh "$target_dir"
+        echo "$version" > "$CURRENT"
+        echo "Finished!"
+    else
+        echo "Failed to download or extract $url"; exit 1
+    fi
+}
+
+# Query the URL and store the response in /tmp.
+echo -e "Querying available versions...\n"
+if ! curl -fsSL -o "$TMP_FILE" "$URL"
+then
+    echo "Failed to download $URL"; exit 1
+fi
+
+# Get LATEST version from response.
+LATEST=$(jq -r '.latest' "$TMP_FILE")
+
+# Get valid VERSIONS from response.
+VERSIONS=$(jq -r '.versions[].version' "$TMP_FILE" | head -10 | awk '{ORS = (NR%5 ? ", " : "\n")} {print}')
+echo -e "Available versions:\n$VERSIONS\n"
+
+echo -e "Enter a specific version (including the v) if you wish to."`
+`"\nPress Enter to download the latest version:"
+
+read INPUT
+
+# Check if the user entered something.
+if [[ -n "$INPUT" ]]
+then
+    # Check if the entered version is valid.
+    if [[ "$VERSIONS" =~ "$INPUT" ]]
+    then
+        download "$INPUT"
+    else
+        echo "Error: $INPUT is not a valid version."
+    fi
+# If the user just press Enter.
+else
+    # If nwjs-version.txt doesn't exist OR if CURRENT version is not LATEST, download the latest version.
+    if [[ ! -f "$CURRENT" ]] || [[ $(cat "$CURRENT") != "$LATEST" ]]
+    then
+        download "$LATEST"
+    else
+        echo "$LATEST is already up to date."
+    fi
+fi
