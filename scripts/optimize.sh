076f8cc035c6e5333aa67db973d05c6f5a0f6a5a
Revival8697 <24849056+Revival8697@users.noreply.github.com>
2024-02-29 18:47:33 +0700

    Make script to work without copying over

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -6,9 +6,23 @@ command -v pngcheck >/dev/null 2>&1 || { echo >&2 "pngcheck is required but it's
 command -v oxipng >/dev/null 2>&1 || { echo >&2 "oxipng is required but it's not installed. Aborting."; exit 1; }
 command -v cwebp >/dev/null 2>&1 || { echo >&2 "cwebp is required but it's not installed. Aborting."; exit 1; }
 
+# Check if the user provided a directory path.
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 path/to/game_dir"; exit 1
+fi
+
+game_dir="${1%/}"
+
+# Check if input path is valid.
+if [[ ! -d $game_dir ]]
+then
+    echo "Input path not valid."; exit 1
+fi
+
 optimize_images()
 {
-    file="$1"
+    local file="$1"
     if pngcheck -v "$file" | grep -q -E "acTL" # Check if a file is an APNG file
     then
         oxipng --opt max --strip safe "$file" || echo >&2 "oxipng failed on $file. Continuing"
@@ -19,27 +33,28 @@ optimize_images()
 
 export -f optimize_images
 
-[ -d ./www ] || { echo >&2 "./www doesn't exist. Aborting."; exit 1; }
+[ -d "$game_dir" ] || { echo >&2 "$game_dir doesn't exist. Aborting."; exit 1; }
 
-if [ ! -d ./www_backup ]
+
+if [ ! -d "$game_dir"/www_backup ]
 then
-    cp -R ./www ./www_backup
+    cp -R "$game_dir"/www "$game_dir"/www_backup
 else
-    echo "./www_backup already exists. Skipping backup."
+    echo "Backup already exists. Skipping backup."
 fi
 
-if [ -f ./www/icon/icon.png ]
+if [ -f "$game_dir"/www/icon/icon.png ]
 then
-    oxipng --opt max --strip all ./www/icon/icon.png
+    oxipng --opt max --strip all "$game_dir"/www/icon/icon.png
 else
-    echo >&2 "./www/icon/icon.png doesn't exist. Ignoring."
+    echo >&2 "Icon doesn't exist. Ignoring."
 fi
 
 if command -v parallel >/dev/null 2>&1
 then
-    find ./www -type f -name "*.png" ! -name "icon.png" | parallel optimize_images
+    find "$game_dir"/www -type f -name "*.png" ! -name "icon.png" | parallel optimize_images
 else
-    find ./www -type f -name "*.png" ! -name "icon.png" -exec optimize_images {} \;
+    find "$game_dir"/www -type f -name "*.png" ! -name "icon.png" -exec optimize_images {} \;
 fi
 
 echo "Finished."
