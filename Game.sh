0eadebfa83f729ac3ddffc232934a862ad992053
Revival8697 <24849056+Revival8697@users.noreply.github.com>
2024-03-28 15:33:26 +0700

    Delete package.json on exit

--- Game.sh
+++ Game.sh
@@ -23,14 +23,20 @@ do
   fi
 done
 
-unmount() {
+clean_up() {
     # Unmount the game files
     if ! fusermount -u "$XDG_DATA_HOME"/nwjs/www
     then
         echo "Error: Failed to unmount."; exit 1
     fi
+
+    # Remove package.json in the game engine folder
+    if ! rm "$XDG_DATA_HOME"/nwjs/package.json
+    then
+        echo "Error: Failed to remove package.json."; exit 1
+    fi
 }
-trap unmount EXIT
+trap clean_up EXIT
 
 # Check if the "www" folder exists
 if [ ! -d "./www" ]
@@ -60,12 +66,6 @@ width=$(echo $resolution | cut -d'x' -f1)
 if ! gamescope -W $height -H $width -F fsr -f -- gamemoderun "$XDG_DATA_HOME"/nwjs/nw
 then
     echo "Error: Failed to launch."; exit 1
-fi
-
-# Remove package.json in the game engine folder
-if ! rm "$XDG_DATA_HOME"/nwjs/package.json
-then
-    echo "Error: Failed to remove package.json."; exit 1
 fi
 
 echo "Finished!"
