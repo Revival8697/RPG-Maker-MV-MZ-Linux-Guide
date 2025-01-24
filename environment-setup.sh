754dca33118d33998644c8d47c6aa413b9b18079
sp1cyf0x <24849056+Revival8697@users.noreply.github.com>
2025-01-24 22:40:54 +0700

    Update DXVK to v2.5.3.

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
 # Check for required commands
-for cmd in wget unzip wine
-do
-  if ! command -v $cmd >/dev/null 2>&1
-  then
-    echo >&2 "$cmd is required but it's not installed. Aborting."; exit 1
+for cmd in wget unzip wine; do
+  if ! command -v "$cmd" >/dev/null 2>&1; then
+    echo >&2 "$cmd is required but it's not installed. Aborting."
+    exit 1
   fi
 done
 
 # Set the target directory
+TARGET_DIR="$XDG_DATA_HOME/nwjs-v0.48.4-win-x64"
 
 # Check if the target directory exists
-if [ -d "$XDG_DATA_HOME/nwjs-v0.48.4-win-x64" ]; then
-  echo "nwjs-v0.48.4-win-x64 is already is its corret location. Exiting."
+if [ -d "$TARGET_DIR" ]; then
+  echo "nwjs-v0.48.4-win-x64 is already in its correct location. Exiting."
   exit 1
 fi
 
 echo "Downloading NW.js archive."
 wget -q -O /tmp/nwjs-v0.48.4-win-x64.zip https://dl.nwjs.io/v0.48.4/nwjs-v0.48.4-win-x64.zip
 
-echo "Extarcting NW.js archive."
+echo "Extracting NW.js archive."
 unzip -q /tmp/nwjs-v0.48.4-win-x64.zip -d "$XDG_DATA_HOME"
 
 echo "Removing temporary file."
 rm /tmp/nwjs-v0.48.4-win-x64.zip
 
 echo "Copying launch script."
-cp ./storage/Game.sh "$XDG_DATA_HOME/nwjs-v0.48.4-win-x64/Game.sh"
+cp ./storage/Game.sh "$TARGET_DIR/Game.sh"
+
+echo "Creating wine prefix."
+wine hostname
+sleep 1
 
 echo "Installing DXVK."
-./storage/dxvk-2.4/setup_dxvk.sh install
+./storage/dxvk-2.5.3/setup_dxvk.sh uninstall
+./storage/dxvk-2.5.3/setup_dxvk.sh install
 
 echo "Finished."
